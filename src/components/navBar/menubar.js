aca08e281becf45b6df9f22b01c0b9831b6cb960
Christoffer A Traeen <christoffer.andersen@live.com>
2022-04-05 22:36:09 +0200

    Fix links menu bar

--- src/components/navBar/menubar.js
+++ src/components/navBar/menubar.js
@@ -4,7 +4,20 @@ import Box from "@mui/material/Box";
 import logo from "../images/Logo-Anette.png";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  margin: 1.5rem 0.75rem;
+  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+  font-weight: 500;
+  font-size: 0.875rem;
+  line-height: 1.75;
+  letter-spacing: 0.02857em;
+  text-transform: uppercase;
+  color: rgba(0, 0, 0, 0.87);
+`;
 
 const MenuBar = () => {
   return (
@@ -14,60 +27,43 @@ const MenuBar = () => {
         style={{
           background: "none",
           card: "none",
-          boxShadow: "none",
+          boxShadow: "none"
         }}
       >
-        <Toolbar
-          disableGutters
-          style={{ display: "flex", flexDirection: "column" }}
-        >
+        <Toolbar disableGutters style={{ display: "flex", flexDirection: "column" }}>
           <Typography noWrap sx={{ flexGrow: 1, display: { xs: "flex" } }}>
-            <Link href="/">
+            <Link to="/">
               <img src={logo} alt="logo" style={{ width: "350px" }} />{" "}
             </Link>
           </Typography>
           <Box
             sx={{
               flexGrow: 2,
-              paddingBottom: "5rem",
+              paddingBottom: "5rem"
             }}
           >
-            <Link
-              href="/"
+            <StyledLink
+              to="/"
               variant="button"
               color="text.primary"
               sx={{
                 my: 1,
                 mx: 1.5,
-                textDecoration: "none",
+                textDecoration: "none"
               }}
             >
               Home
-            </Link>
-            <Link
+            </StyledLink>
+            <StyledLink to="/about">About</StyledLink>
+            <StyledLink to="/projects">Projects</StyledLink>
+            <StyledLink
               variant="button"
               color="text.primary"
-              href="/about"
-              sx={{ my: 1, mx: 1.5, textDecoration: "none" }}
-            >
-              About
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="/projects"
-              sx={{ my: 1, mx: 1.5, textDecoration: "none" }}
-            >
-              Projects
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="/contact"
+              to="/contact"
               sx={{ my: 1, mx: 1.5, textDecoration: "none" }}
             >
               Contact
-            </Link>
+            </StyledLink>
           </Box>
         </Toolbar>
       </AppBar>
